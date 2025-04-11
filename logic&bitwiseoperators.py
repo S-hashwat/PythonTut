a76d47914aafac18a5bb916bfca21c705d6d129a
Shashwat <shashwat.saxena2020@vitbhopal.ac.in>
2025-04-11 23:04:28 +0530

    logical operator updated

--- logic&bitwiseoperators.py
+++ logic&bitwiseoperators.py
@@ -1,4 +1,4 @@
-# Operators - 
+# LOGICAL Operators - 
 # AND , OR Operators
 
 # AND Operator Chart 
@@ -13,10 +13,19 @@
 # TRUE + TRUE = True
 # FALSE + FALSE = FALSE
 
-Age1 = 19
-Age2 = 25
+# Age1 = 19
+# Age2 = 25
 
-if(Age1 and Age2 >= 18):
-    print("The person is adult")
-elif(Age1 or Age2 <= 18 ):
-    print("The person is not adult ")
+# if(Age1 and Age2 >= 18):
+#     print("The person is adult")
+# elif(Age1 or Age2 <= 18 ):
+#     print("The person is not adult ")
+
+# is_hungry = True
+# if(not is_hungry):
+#   print("You are not hungry")
+# else:
+#   print("You are hungry")
+
+
+#BITWISE OPERATORS
